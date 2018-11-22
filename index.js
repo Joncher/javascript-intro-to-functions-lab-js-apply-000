d748c167936855d8ff4b2a729fa8e3e90a9d88ef
John Crespi Oncher <johnoncher@gmail.com>
2018-11-22 15:34:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ function logWhisper(String){
 function sayHiToGrandma(String){
   if (String === whisper(String))
   {return "I can't hear you!"}
-
+  
   if (String === shout(String));
  { return "YES INDEED!"}
-
+ 
  if (String === "I love you, Grandma."){
    return "I love you, too."
  }
